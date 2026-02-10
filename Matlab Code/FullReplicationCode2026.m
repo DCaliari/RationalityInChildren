fd6ff2224f6438e376f3833d1c8fde6264d74a69
DCaliari <68252050+DCaliari@users.noreply.github.com>
2026-02-10 20:20:30 +0100

    Add files via upload

---
+++ Matlab Code/FullReplicationCode2026.m
@@ -0,0 +1,365 @@
+%% Figure 4 - Cumulative distributions of parental education %%
+
+Figure4_ParentalEducation
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure4')
+
+clear
+clc
+close all
+
+%% Figure 5 - Percentage of rational pupils %%
+
+Figure5_HeatmapsNbcAcLotteries
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure5')
+
+clear
+clc
+close all
+
+%% Figure 6a - Distribution of preferences by school (pencils) %%
+
+Figure6a_DistributionPreferencesSchoolsPencils
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure6a')
+
+clear
+clc
+close all
+
+%% Figure 6b - Distribution of preferences by gender (pencils) %%
+
+Figure6b_DistributionPreferencesGenderPencils
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure6b')
+
+clear
+clc
+close all
+
+
+%% Figure 6 - ChiSquare tests %%
+
+Figure6_chisquaretest
+writetable(section_4_3_chisquare_pencils,'Matlab Code/figures_tables/stored_figures_tables/section_4_3_chisquare_pencils.csv')
+
+clear
+clc
+close all
+
+
+%% Figure 7a - Consideration parameter by school and grade (pencils) %%
+
+Figure7a_ConsiderationParameterSchoolsPencils
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure7a')
+
+writetable(section_5_2_1,'Matlab Code/figures_tables/stored_figures_tables/Section5_2_1.csv')
+writetable(section_5_2_2,'Matlab Code/figures_tables/stored_figures_tables/Section5_2_2.csv')
+
+clear
+clc
+close all
+
+%% Figure 7b - Consideration parameter by gender and grade (pencils) %%
+
+Figure7b_ConsiderationParameterGenderPencils
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure7b')
+
+writetable(section_5_2_11,'Matlab Code/figures_tables/stored_figures_tables/Section5_2_11.csv')
+writetable(section_5_2_21,'Matlab Code/figures_tables/stored_figures_tables/Section5_2_21.csv')
+
+clear
+clc
+close all
+
+%% Figure 8 - Joint distribution Coin drop game %%
+
+Figure8_NoBinaryCycleAlwaysChosen
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure8')
+
+clear
+clc
+close all
+
+
+%% Figure 9 - Consideration parameter independent task %%
+
+Figure9_ConsiderationParameterIndependent
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure9')
+
+writetable(section_5_3_11,'Matlab Code/figures_tables/stored_figures_tables/Section5_3_11.csv')
+writetable(section_5_3_12,'Matlab Code/figures_tables/stored_figures_tables/Section5_3_12.csv')
+
+
+clear
+clc
+close all
+
+%% Figure 10 - Index of correlation neglect %%
+
+Figure10_IndexCorrelationNeglect
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure10')
+
+writetable(section_5_3_21,'Matlab Code/figures_tables/stored_figures_tables/Section5_3_21.csv')
+writetable(section_5_3_22,'Matlab Code/figures_tables/stored_figures_tables/Section5_3_22.csv')
+writetable(section_5_3_22,'Matlab Code/figures_tables/stored_figures_tables/Section5_3_23.csv')
+
+clear
+clc
+close all
+
+%% Figure 11 - Raven development %%
+
+Figure11_Raven
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure11')
+
+writetable(section_7,'Matlab Code/figures_tables/stored_figures_tables/Section7.csv')
+
+clear
+clc
+close all
+
+%% Figure 22 - Joint distribution Coin Drop game
+
+Figure22_JointDistributionCoinDropGame
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure22')
+
+clear
+clc
+close all
+
+%% Figure 23a - Distribution Preferences by schools (pens) %%
+
+Figure23a_DistributionPreferencesSchoolsPens
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure23a')
+
+clear
+clc
+close all
+
+
+%% Figure 23b - Distribution Preferences by gender (pens) %%
+
+Figure23b_DistributionPreferencesGenderPens
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure23b')
+
+clear
+clc
+close all
+
+
+%% Figure 23 - ChiSquare tests %%
+
+Figure6_chisquaretest
+writetable(section_A1OA_chisquare_pens,'Matlab Code/figures_tables/stored_figures_tables/section_A1OA_chisquare_pens.csv')
+
+clear
+clc
+close all
+
+
+%% Figure 24a - Consideration parameter by schools (pens) %%
+
+Figure24a_ConsiderationParameterSchoolsPens
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure24a')
+
+writetable(section_A1OA_1,'Matlab Code/figures_tables/stored_figures_tables/Section_A1OA_1.csv')
+writetable(section_A1OA_1_2,'Matlab Code/figures_tables/stored_figures_tables/Sectio_A1OA_1_2.csv')
+
+
+clear
+clc
+close all
+
+%% Figure 24b - Consideration parameter by genders (pens) %%
+
+Figure24b_ConsiderationParameterGenderPens
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure24b')
+
+writetable(section_A1OA_11,'Matlab Code/figures_tables/stored_figures_tables/Section_A1OA_11.csv')
+writetable(section_A1OA_21,'Matlab Code/figures_tables/stored_figures_tables/Sectio_A1OA_21.csv')
+
+
+clear
+clc
+close all
+
+
+
+
+%% Figure 25 - Evolution consideration parameter and preference intensity %%
+
+Figure25_EvolutionConsiderationAndPreferenceIntensity
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure25')
+
+clear
+clc
+close all
+
+%% Figure 27 - Risk by gender %%
+
+Figure27_RiskGender
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure27')
+
+clear
+clc
+close all
+
+%% Figure 29 - Distribution of choice in the first coin-drop game %%
+
+Figure29_DistributionChoicesFirstPachinko
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure29')
+
+clear
+clc
+close all
+
+%% Figure 30 - Choices in the first pachinko %%
+
+Figure30_FirstPachinkoChoices
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure30')
+
+clear
+clc
+close all
+
+
+
+%% Figure 31 - Distribution preferences by schools (pencils and pens) %%
+
+Figure31_DistributionPreferencesBySchoolsAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure31')
+
+clear
+clc
+close all
+
+%% Figure 31 and 34 - ChiSquare tests %%
+
+Figure31_34_chisquaretest
+writetable(section_B1OA_chisquare_gender,'Matlab Code/figures_tables/stored_figures_tables/section_B1OA_chisquare_gender.csv')
+writetable(section_B1OA_chisquare_schools,'Matlab Code/figures_tables/stored_figures_tables/section_B1OA_chisquare_schools.csv')
+
+clear
+clc
+close all
+
+%% Figure 32 - Consideration parameter by schools (pencils and pens) %%
+
+Figure32_ConsiderationParameterAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure32')
+
+clear
+clc
+close all
+
+
+%% Figure 33 - Consideration parameter by gender (pencils and pens) %%
+
+Figure33_ConsiderationParameterGendersAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure33')
+
+clear
+clc
+close all
+
+%% Figure 34 - Distribution preferences by gender (pencils and pens) %%
+
+Figure34_DistributionPreferencesGenderAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure34')
+
+clear
+clc
+close all
+
+%% Figure 35 - Joint distribution coin drop game in all schools %%
+
+Figure35_JointDistributionCoinDropGameAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure35')
+
+clear
+clc
+close all
+
+%% Figure 36 - Consideration parameter coin drop game in all schools %%
+
+Figure36_ConsiderationParameterCoinDropAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure36')
+
+clear
+clc
+close all
+
+%% Figure 37 - Raven development all schools %%
+
+Figure37_RavenAllSchools
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure37')
+
+clear
+clc
+close all
+
+%% Figure 38 - Consideration parameter all models specifications %%
+
+Figure38_ConsiderationParameterAllModelSpecifications
+saveas(gcf,'Matlab Code/figures_tables/stored_figures_tables/Figure38')
+
+clear
+clc
+close all
+
+
+%% Table 2 - Menu and Framing Effects %%
+
+Table2_AC_Pachinko
+writetable(TABLE2,'Matlab Code/figures_tables/stored_figures_tables/Table2.csv')
+
+clear
+clc
+close all
+
+%% Table 3 - Independent task %%
+
+Table3_Independent
+writetable(TABLE3,'Matlab Code/figures_tables/stored_figures_tables/Table3.csv')
+
+clear
+clc
+close all
+
+%% Table 4 - Correlated task %%
+
+Table4_Correlated
+writetable(TABLE4,'Matlab Code/figures_tables/stored_figures_tables/Table4.csv')
+
+clear
+clc
+close all
+
+%% Table 5 - Odds ratios %%
+
+Table5_Oddsratios
+writetable(TABLE5_1,'Matlab Code/figures_tables/stored_figures_tables/Table5_1.csv')
+writetable(TABLE5_2,'Matlab Code/figures_tables/stored_figures_tables/Table5_2.csv')
+
+clear
+clc
+close all
+
+%% Tables 6 7 - Women Experiment %%
+
+Tables6_7_WomenExperiment
+writetable(TABLE6,'Matlab Code/figures_tables/stored_figures_tables/Table6.csv')
+writetable(TABLE7,'Matlab Code/figures_tables/stored_figures_tables/Table7.csv')
+
+clear
+clc
+close all
+
+
+%% Tables 8 9 - NBC and AC joint distribution %%
+
+Tables8_9_NbcAcContingencyTables
+writetable(TABLE8,'Matlab Code/figures_tables/stored_figures_tables/Table8.csv')
+writetable(TABLE9,'Matlab Code/figures_tables/stored_figures_tables/Table9.csv')
+
+clear
+clc
+close all
